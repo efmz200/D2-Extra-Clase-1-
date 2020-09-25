1b218ffd1eecb812ef8cd78f84ad82864541c9d3
Erick <efmz2000@outlook.es>
2020-09-24 19:28:05 -0600

    arreglo del grafo para el servidor

--- Grafo.h
+++ Grafo.h
@@ -5,7 +5,7 @@
 #ifndef EXTRACLASE_1_GRAFO_H
 
 #define EXTRACLASE_1_GRAFO_H
-#define INF 9999
+#define INF 999
 #include "Vertice.h"
 #include "Arista.h"
 using namespace std;
@@ -66,7 +66,7 @@ class Grafo {
             }
 
         }
-        void genmatriz(){
+        string genmatriz(){
             int largo;//cantidad de nodos en el grafo
             largo=lengrafo();
             largo++;
@@ -81,60 +81,35 @@ class Grafo {
             }
             //for encargado de llenar de infinitos la matriz
             for (int i=1;i<largo;i++){
-
                 for (int a=1;a<largo;a++){
-
                     arr[i][a]=to_string(INF);
-
                     if(a==i){//if para poner 0 en la diagonal principal
                         arr[i][a]="0";
                     }
                 }
             }
             aux=inicial;
-
             //Bloque de codigo para poder asignar los pesos de las aristas en la matriz
             for (int i=1;i<largo;i++){
                 Arista *auxArt=aux->arist;
-
                 while(auxArt!=nullptr){
                     for(int a=1;a<largo;a++){
-                        if(a==i){
-                            continue;
-                        }
+                        if(a==i){continue;}
                         Vertice *auxVert=auxArt->referencia;
-
                         if (arr[0][a]==auxVert->nom){
                             arr[i][a]=to_string(auxArt->peso);
-
                             break;
-
                         }
-
                     }
                     auxArt=auxArt->sig;
                 }
                 aux=aux->sig;
             }
-
-
-            //--------------
-            //print de pueba
-            //---------------
-            for (int i=0;i<largo;i++){
-                std::cout << "-"<<std::endl;
-                for (int a=0;a<largo;a++){
-                    std::cout << "_";
-                    std::cout << arr[i][a] ;
-                }
-            }
             cout<<std::endl;
             //Floyd Warshal;
-            cout<<"prueba "<<stoi(arr[1][2])<<std::endl;
             for (int a=1;a<largo;a++){
                 for (int i=1;i<largo;i++){
                     for (int j=1;j<largo;j++){
-
                         if(stoi(arr[i][a])+stoi(arr[a][j])<stoi(arr[i][j])){
                             int suma=stoi(arr[i][a])+stoi(arr[a][j]);
                             arr[i][j]=to_string(suma);
@@ -142,18 +117,123 @@ class Grafo {
                     }
                 }
             }
-
-            //--------------
-            //print de pueba
-            //---------------
             for (int i=0;i<largo;i++){
-                std::cout << "-"<<std::endl;
-                for (int a=0;a<largo;a++){
-                    std::cout << "_";
-                    std::cout << arr[i][a] ;
+                salida+="| ";
+                for (int j=0;j<largo;j++){
+                    if (arr[i][j]==""){
+                        salida+=arr[i][j];
+                        salida+=("   |  ");
+                        continue;
+                    }
+                    if (arr[i][j].length()<3){
+                        salida+=arr[i][j];
+                        salida+=("  |  ");
+                        continue;
+                    }
+                    if(arr[i][j]=="999"){
+                        salida.pop_back();
+                        salida+=" INF";
+                        salida+=(" | ");
+                        continue;
+                    }
+                    salida+=arr[i][j];
+                    salida+=(" | ");
+                    //cout<<arr[i][j];
+                }
+                salida+="\n";
+            }
+            cout<<salida;
+            return salida;
+
+        }
+    void prueba_matriz(){
+        int largo;//cantidad de nodos en el grafo
+        largo=lengrafo();
+        largo++;
+        string arr[largo] [largo];
+        string salida;
+        Vertice *aux=inicial;
+        // for para poner el nombre del nodo en la primer fila y columna
+        for (int i=1;i<largo;i++){
+            arr[i][0]=aux->nom;
+            arr[0][i]=aux->nom;
+            aux=aux->sig;
+        }
+        //for encargado de llenar de infinitos la matriz
+        for (int i=1;i<largo;i++){
+
+            for (int a=1;a<largo;a++){
+
+                arr[i][a]=to_string(INF);
+
+                if(a==i){//if para poner 0 en la diagonal principal
+                    arr[i][a]="0";
                 }
             }
         }
+        aux=inicial;
+
+        //Bloque de codigo para poder asignar los pesos de las aristas en la matriz
+        for (int i=1;i<largo;i++){
+            Arista *auxArt=aux->arist;
+
+            while(auxArt!=nullptr){
+                for(int a=1;a<largo;a++){
+                    if(a==i){
+                        continue;
+                    }
+                    Vertice *auxVert=auxArt->referencia;
+
+                    if (arr[0][a]==auxVert->nom){
+                        arr[i][a]=to_string(auxArt->peso);
+
+                        break;
+
+                    }
+
+                }
+                auxArt=auxArt->sig;
+            }
+            aux=aux->sig;
+        }
+
+
+        //--------------
+        //print de pueba
+        //---------------
+        for (int i=0;i<largo;i++){
+            std::cout << "-"<<std::endl;
+            for (int a=0;a<largo;a++){
+                std::cout << "_";
+                std::cout << arr[i][a] ;
+            }
+        }
+        cout<<std::endl;
+        //Floyd Warshal;
+        cout<<"prueba "<<stoi(arr[1][2])<<std::endl;
+        for (int a=1;a<largo;a++){
+            for (int i=1;i<largo;i++){
+                for (int j=1;j<largo;j++){
+
+                    if(stoi(arr[i][a])+stoi(arr[a][j])<stoi(arr[i][j])){
+                        int suma=stoi(arr[i][a])+stoi(arr[a][j]);
+                        arr[i][j]=to_string(suma);
+                    }
+                }
+            }
+        }
+
+        //--------------
+        //print de pueba
+        //---------------
+        for (int i=0;i<largo;i++){
+            std::cout << "-"<<std::endl;
+            for (int a=0;a<largo;a++){
+                std::cout << "_";
+                std::cout << arr[i][a] ;
+            }
+        }
+    }
     };
 
 #endif //EXTRACLASE_1_GRAFO_H
